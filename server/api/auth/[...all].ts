e944a5fcd3e98d1f6dd77c3b62fc8e4d92b6d5d6
Anastasia <cookiedemons@outlook.com>
2026-06-28 18:34:45 -0400

    fix: syntax

--- server/api/auth/[...all].ts
+++ server/api/auth/[...all].ts
@@ -2,8 +2,7 @@ import { env } from '~~/server/env'
 import { auth } from '~~/server/utils/auth'
 
 export default defineEventHandler(async (event) => {
-  const inbound = new URL(event.req.url)
-  const url = new URL(inbound.pathname + inbound.search, env.BETTER_AUTH_URL)
+  const url = new URL(event.req.url, env.BETTER_AUTH_URL)
 
   const init: RequestInit = {
     method: event.req.method,
