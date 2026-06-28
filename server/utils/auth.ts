20d9733ed97ce8eb0f445c80698e9c9d1e3c64fa
Anastasia <cookiedemons@outlook.com>
2026-06-28 16:53:39 -0400

    feat: additional environment setup for prod/dev work

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -2,8 +2,15 @@ import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { getDb } from '~~/server/database/db'
 import * as schema from '~~/server/database/schemas/index'
+import { env } from '~~/server/env'
 
 export const auth = betterAuth({
+  baseURL: env.BETTER_AUTH_URL,
+  secret: env.BETTER_AUTH_API_KEY,
+  trustedOrigins: [
+    new URL(env.BETTER_AUTH_URL).origin,
+    ...(env.NODE_ENV === 'development' ? ['http://localhost:3000'] : []),
+  ],
   database: drizzleAdapter(getDb(), {
     provider: 'pg',
     schema,
