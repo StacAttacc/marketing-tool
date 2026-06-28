d3e690732e865e57ada5a3bdd227f2d97115150e
Anastasia <cookiedemons@outlook.com>
2026-06-28 18:30:02 -0400

    feat: using array buffer for login

--- server/api/auth/[...all].ts
+++ server/api/auth/[...all].ts
@@ -1,7 +1,17 @@
 import { env } from '~~/server/env'
 import { auth } from '~~/server/utils/auth'
 
-export default defineEventHandler((event) => {
-  const url = new URL(event.req.url, env.BETTER_AUTH_URL)
-  return auth.handler(new Request(url, event.req))
+export default defineEventHandler(async (event) => {
+  const inbound = new URL(event.req.url)
+  const url = new URL(inbound.pathname + inbound.search, env.BETTER_AUTH_URL)
+
+  const init: RequestInit = {
+    method: event.req.method,
+    headers: event.req.headers,
+  }
+  if (event.req.method !== 'GET' && event.req.method !== 'HEAD') {
+    init.body = await event.req.arrayBuffer()
+  }
+
+  return auth.handler(new Request(url, init))
 })
